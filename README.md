3e0adf078ba44d5686f9d392a24d44a37d70cc34
azunino97/home <zunino.anthony@gmail.com>
2023-12-10 11:18:14 -0800

    Updating README.txt

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # CreditCardReader
+
+Simple CreditCard Reader that will automatically take in a file, determine the file type, then output the validated cards to a user specified file.
+
+In order to run ccParser.java, update the input file names and output file names for the types of files you want read, then run the program. The files will output to wherever the user specifies.
+
+> CreditCardReader
+>> ccParser.java
+>> CreditCard.java
+>> CreditCardParser.java
+>> InputOutputPaths.java
+>> Parser.java
+>> ParserCsv.java
+>> ParserJson.java
+>> ParserXml.java
+>> TestAssertions.java
+>> TestRunner.java
+>> InputOutput
+>>> input_file.csv
+>>> input_file.xml
+>>> input_file.json
+>>> output_file.csv
+>>> output_file.xml
+>>> output_file.json
--- ccParser.java
+++ ccParser.java
@@ -2,13 +2,22 @@ import java.util.Vector;
 
 class Main {
 	public static void main(String[] args) {
-		String inputPathCsv = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/input_file.csv";
-		String inputPathXml = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/input_file.xml";
-		String inputPathJson = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/input_file.json";
+		// String inputPathCsv = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/input_file.csv";
+		// String inputPathXml = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/input_file.xml";
+		// String inputPathJson = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/input_file.json";
 
-		String outputPathCsv = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/output_file.csv";
-		String outputPathXml = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/output_file.xml";
-		String outputPathJson = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/output_file.json";
+		// String outputPathCsv = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/output_file.csv";
+		// String outputPathXml = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/output_file.xml";
+		// String outputPathJson = "/Users/q/Documents/GitHub/CreditCardReader/inputOutput/output_file.json";
+
+
+		String inputPathCsv = "/Users/q/Documents/GitHub/CreditCardReader/FinalDemo/Fall23-Section-01-Inputs/inputs/demo1_inp.csv";
+		String inputPathXml = "/Users/q/Documents/GitHub/CreditCardReader/FinalDemo/Fall23-Section-01-Inputs/inputs/demo1_inp.xml";
+		String inputPathJson = "/Users/q/Documents/GitHub/CreditCardReader/FinalDemo/Fall23-Section-01-Inputs/inputs/demo1_inp.json";
+
+		String outputPathCsv = "/Users/q/Documents/GitHub/CreditCardReader/FinalDemo/output_inp.csv";
+		String outputPathXml = "/Users/q/Documents/GitHub/CreditCardReader/FinalDemo/output_inp.xml";
+		String outputPathJson = "/Users/q/Documents/GitHub/CreditCardReader/FinalDemo/output_inp.json";
 
 		Vector<InputOutputPaths> paths = new Vector<InputOutputPaths>();
 		paths.add(new InputOutputPaths(inputPathCsv, outputPathCsv));
